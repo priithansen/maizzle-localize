8de9e0b22abf8b145e69e91a81b4638282fb2cfd
Priit <priit@hpr.ee>
2023-03-30 13:07:38 +0300

    Simple i18next integration example

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const i18next = require('i18next')
+
 /*
 |-------------------------------------------------------------------------------
 | Development config                      https://maizzle.com/docs/environments
@@ -10,6 +12,8 @@
 */
 
 module.exports = {
+  lang: 'en', // Use as fallback if nothing specified with env
+  t: i18next.t, // Make i18next translate function available in templates as page.t()
   build: {
     templates: {
       source: 'src/templates',
@@ -20,6 +24,36 @@ module.exports = {
         source: 'src/images',
         destination: 'images',
       },
+    },
+  },
+  events: {
+    beforeCreate: async (config) => {
+      const fallbackLng = config.lang
+
+      if (process.env.MAIZZLE_TEMPLATE_LANG) {
+        config = Object.assign(config, { 
+          lang: process.env.MAIZZLE_TEMPLATE_LANG 
+        })
+      }
+
+      // Await is important here to not let build advance and start using page.t() before i18next has finished initializing.
+      await i18next.init({
+        lng: config.lang,
+        fallbackLng,
+        debug: false,
+        resources: { // Inlined just to simplify example.
+          en: {
+            translation: {
+              "hello": "Hello {{name}}!",
+            },
+          },
+          et: {
+            translation: {
+              "hello": "Tere {{name}}!",
+            },
+          },
+        },
+      })
     },
   },
 }
